89d04ffc1a492b5cee6873183eb12b2ce2ca111d
Olimpiada Brasileira de Informatica <obi81@pernalonga.lab.ic.unicamp.br>
2020-01-30 17:49:17 -0200

    Fix StringHasing

--- String/StringHasing.cpp
+++ String/StringHasing.cpp
@@ -18,16 +18,16 @@ ll hashp[N];
 // Generates a hash prefix sum of a string
 void hash_string(const string& str)
 {
-	hashp[0] = 0;
 	for (int i = 0; i < (int)str.size(); i++)
 	{
-		hashp[i + 1] = (hashp[i] + (str[i] * e(i)) % mod) % mod;
+		hashp[i + 1] = ((hashp[i] * b) % mod + str[i]) % mod;
 	}
 }
 
 ll get_hash(int l, int r)
 {
-	ll v = hashp[r + 1] - hashp[l];
+	ll v = hashp[r + 1];
+	v = (v - (hashp[l] * e(r - l + 1)) % mod) % mod;
 	return v < 0 ? v + mod : v;
 }
 
@@ -36,12 +36,12 @@ int main()
 	string s;
 	cin>>s;
 	hash_string(s);
-	for (int i = 0; i <= s.size(); i++) cout<<hashp[i]<<' ';
-	cout<<endl;
 	while (true)
 	{
 		int va, vb;
 		cin>>va>>vb;
-		cout<<"Hash of string ["<<va<<", "<<vb<<"] = "<<get_hash(va, vb)<<endl;
+		cout<<"Hash of string ["<<va<<", "<<vb<<"] ("
+			<<s.substr(va, vb - va + 1)
+			<<") = "<<get_hash(va, vb)<<endl;
 	}
 }
